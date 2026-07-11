abb81e0fb965486abf3e3f60cbae90ad6c270950
Dzmitry Zayats <dzzayats@student.42warsaw.pl>
2026-07-11 09:17:53 +0200

    Some weird stuff

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dzzayats <dzzayats@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 23:21:54 by dzzayats          #+#    #+#             */
-/*   Updated: 2026/07/11 00:46:49 by dzzayats         ###   ########.fr       */
+/*   Updated: 2026/07/11 09:16:39 by dzzayats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,73 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int max(int num_args, ...);
-
-int main ()
+static  int     helper_func(const char f, va_list arg)
 {
-       int max_num =  max (4, 10,12, 14,16, 25, 3,120);
-        printf("max_num %d\n", max_num);
+        int     len;
+        
+        len = 0;
+	if (f == '%')
+		len = ft_putchar('%');
+	else if (f == 'c')
+		len = ft_putchar(va_arg(arg, int));
+        return (len);
 }
 
-int max(int num_args, ...)
+int my_printf(const char *format, ...)
 {
         va_list args;
-        va_start(args, num_args);
-
-        int max = 0;
-        int i = 0;
-        while (i < num_args)
-        {
-                int x = va_arg(args, int);
-                if (i == 0) max = x;
-                else if (x > max) max = x;
-                i++;
-        } 
-        va_end(args);
+        int     input_chars;
+        int     i;
         
-        return (max);
+        input_chars = 0;
+        i = 0;
+        if (!format)
+                return (-1);
+        va_start (args,format);
+        while (format[i])
+        {
+                if (format[i] != 0)
+                {
+                        
+                }
+                i++;
+        }
+        
+
+        va_end (args);
+        return (input_chars);
 }
+        
+
+int main()
+{
+        my_printf("%c", 'c');
+        
+}
+
+// #include <stdio.h>
+// #include <stdarg.h>
+
+// void mini_printf(const char *format, ...) {
+//     va_list args;
+//     va_start(args, format);
+
+//     for (int i = 0; format[i] != '\0'; i++) {
+//         // Check for the %c placeholder
+//         if (format[i] == '%' && format[i + 1] == 'c') {
+//             // Retrieve as int due to default argument promotion
+//             char c = (char)va_arg(args, int); 
+//             putchar(c);
+//             i++; // Skip the 'c' character
+//         } else {
+//             putchar(format[i]); // Print regular characters
+//         }
+//     }
+
+//     va_end(args);
+// }
+
+// int main() {
+//     mini_printf("Hello %c%c%c\n", 'A', 'B', '!');
+//     return 0;
+// }
